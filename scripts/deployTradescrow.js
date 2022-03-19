0928ed71782ce0934f34f64e7c11d2938b8fd425
Nicholas St. Germain <nick@cajun.pro>
2022-03-19 04:41:57 -0500

    cleanup deployment

--- scripts/deployTradescrow.js
+++ scripts/deployTradescrow.js
@@ -2,30 +2,25 @@ const hre = require("hardhat");
 const { ethers } = require("hardhat")
 require("dotenv").config()
 
-async function deploy(name, args) {
+async function deploy(name) {
     // Deploying
     console.log("Starting deployment...")
     const contractFactory = await ethers.getContractFactory(name)
     console.log("Deploying " + name)
-    const contract = await contractFactory.deploy(...args)
+    const contract = await contractFactory.deploy()
     console.log(name + " deployed! Address:", contract.address)
-    return {name, args, address: contract.address}
+    return {name, address: contract.address}
 }
 
 async function verify(contract) {
     console.log("Verifying " + contract.name)
     await hre.run("verify:verify", {
-        address: contract.address,
-        constructorArguments: contract.args,
+        address: contract.address
     })
 }
+
 async function main() {
-    const signers = await ethers.getSigners()
-    const constructorArguments = [
-        "1000000000000000000",
-        signers[0].address
-    ]
-  await deploy("Tradescrow", constructorArguments)
+  await deploy("Tradescrow")
       .then(async (contract) => await verify(contract))
 }
 
