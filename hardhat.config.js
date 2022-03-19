c4221d37dfc1fab6070a191c2e1c3808d0898de7
Nicholas St. Germain <nick@cajun.pro>
2022-03-19 15:53:46 -0500

    compile

--- hardhat.config.js
+++ hardhat.config.js
@@ -42,19 +42,44 @@ module.exports = {
       chainId: 1666700000,
       accounts: [process.env.PRIVATE_KEY]
     },
+    bsc: {
+      url: 'https://bsc-dataseed1.binance.org',
+      chainId: 56,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    bscTestnet: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      chainId: 97,
+      accounts: [process.env.PRIVATE_KEY]
+    },
     avalanche: {
       url: 'https://api.avax.network/ext/bc/C/rpc',
       chainId: 43114,
       accounts: [process.env.PRIVATE_KEY]
     },
-    fantom: {
+    avalancheFujiTestnet: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      chainId: 43113,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    opera: {
       url: 'https://rpc.ftm.tools',
       chainId: 250,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    ftmTestnet: {
+      url: 'https://rpc.testnet.fantom.network',
+      chainId: 4002,
       accounts: [process.env.PRIVATE_KEY]
     },
     polygon: {
       url: 'https://polygon-rpc.com',
       chainId: 137,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    polygonMumbai: {
+      url: 'https://rpc-mumbai.maticvigil.com',
+      chainId: 80001,
       accounts: [process.env.PRIVATE_KEY]
     }
   },
@@ -63,8 +88,14 @@ module.exports = {
           harmony: 'not needed',
           harmonyTest: 'not needed',
           avalanche: process.env.SNOWTRACE_API_KEY,
+          avalancheFujiTestnet: process.env.SNOWTRACE_API_KEY,
           opera: process.env.FTMSCAN_API_KEY,
-          polygon: process.env.POLYGONSCAN_API_KEY
+          ftmTestnet: process.env.FTMSCAN_API_KEY,
+          polygon: process.env.POLYGONSCAN_API_KEY,
+          polygonMumbai: process.env.POLYGONSCAN_API_KEY,
+          bsc: process.env.BSCSCAN_API_KEY,
+          bscTestnet: process.env.BSCSCAN_API_KEY,
+
         }
     }
 };
