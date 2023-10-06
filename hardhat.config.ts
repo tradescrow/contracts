9a21a68d5617630d0856ea11b12b45b7b5192b1c
Nicholas St. Germain <nick@cajun.pro>
2023-10-06 14:31:40 -0500

    sync

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,41 +1,147 @@
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@openzeppelin/hardhat-upgrades";
+import * as tenderly from "@tenderly/hardhat-tenderly";
+import '@nomicfoundation/hardhat-verify';
 
+import "@dirtycajunrice/hardhat-tasks/internal/type-extensions"
+import "@dirtycajunrice/hardhat-tasks";
 import "dotenv/config";
+import "./tasks";
+import '@openzeppelin/hardhat-upgrades';
 
-import "./tasks/deployUpgradeable";
-import "./tasks/getUpgradeDetails";
-import "./tasks/upgrade";
+import { NetworksUserConfig } from "hardhat/types";
 
-const settings = {
-  optimizer: {
-    enabled: true,
-    runs: 200,
+tenderly.setup({ automaticVerifications: false });
+
+const networkData = [
+  {
+    name: "mainnet",
+    chainId: 1,
+    urls: {
+      rpc: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      api: "",
+      browser: "",
+    },
   },
-};
-const accounts = [ process.env.PRIVATE_KEY_2 ];
+  {
+    name: "arbitrumOne",
+    chainId: 42_161,
+    urls: {
+      rpc: `https://arb1.arbitrum.io/rpc`,
+      api: "",
+      browser: "",
+    },
+  },
+  {
+    name: "avalanche",
+    chainId: 43_114,
+    urls: {
+      rpc: `https://rpc.ankr.com/avalanche`,
+      api: "",
+      browser: "",
+    },
+  },
+  {
+    name: "boba",
+    chainId: 288,
+    urls: {
+      rpc: `https://mainnet.boba.network`,
+      api: "https://api.bobascan.com/api",
+      browser: "https://bobascan.com",
+    },
+  },
+  {
+    name: "base",
+    chainId: 8_453,
+    urls: {
+      rpc: `https://developer-access-mainnet.base.org`,
+      api: "https://api.basescan.org/api",
+      browser: "https://basescan.org/",
+    },
+  },
+  {
+    name: "polygon",
+    chainId: 137,
+    urls: {
+      rpc: `https://polygon-mainnet.public.blastapi.io`,
+      api: "",
+      browser: "",
+    },
+  },
+  {
+    name: "polygonZkevm",
+    chainId: 1_101,
+    urls: {
+      rpc: `https://zkevm-rpc.com`,
+      api: "https://api-zkevm.polygonscan.com/api",
+      browser: "https://zkevm.polygonscan.com/",
+    },
+  },
+  {
+    name: "optimisticEthereum",
+    chainId: 10,
+    urls: {
+      rpc: `https://mainnet.optimism.io`,
+      api: "",
+      browser: "",
+    },
+  },
+  {
+    name: "bsc",
+    chainId: 56,
+    urls: {
+      rpc: `https://bsc-rpc.gateway.pokt.network`,
+      api: "",
+      browser: "",
+    },
+  },
+  {
+    name: "dfk",
+    chainId: 53_935,
+    urls: {
+      rpc: `https://subnets.avax.network/defi-kingdoms/dfk-chain/rpc`,
+      api: "https://api.avascan.info/v2/network/mainnet/evm/53935/etherscan",
+      browser: "https://avascan.info/blockchain/dfk/",
+    },
+    accounts: [ process.env.PRIVATE_KEY! ],
+  },
+];
+
 module.exports = {
   solidity: {
-    compilers: [ "8.19", "8.9", "8.2", "6.0" ].map(v => (
-      { version: `0.${v}`, settings }
-    )),
+    compilers: [ "8.20", "8.9", "8.2", "6.0" ].map(v => ({
+      version: `0.${v}`,
+      settings: { ...(v === "8.20" ? { evmVersion: "london" } : {} ), optimizer: { enabled: true, runs: 200 } },
+    })),
+    overrides: {
+      "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol": {
+        version: "0.8.9",
+        settings: { optimizer: { enabled: true, runs: 200 } },
+      },
+      "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol": {
+        version: "0.8.9",
+        settings: { optimizer: { enabled: true, runs: 200 } },
+      },
+      "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol": {
+        version: "0.8.9",
+        settings: { optimizer: { enabled: true, runs: 200 } },
+      },
+      "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol": {
+        version: "0.8.9",
+        settings: { optimizer: { enabled: true, runs: 200 } },
+      },
+      "contracts/proxy.sol": {
+        version: "0.8.9",
+        settings: { optimizer: { enabled: true, runs: 200 } },
+      },
+    },
   },
-  networks: {
-    mainnet: { url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`, chainId: 1, accounts },
-    arbitrumOne: { url: "https://arb1.arbitrum.io/rpc", chainId: 42161, accounts: accounts },
-    avalanche: { url: "https://rpc.ankr.com/avalanche", chainId: 43114, accounts },
-    boba: { url: "https://mainnet.boba.network", chainId: 288, accounts },
-    base: { url: "https://developer-access-mainnet.base.org", chainId: 8453, accounts },
-    polygon: { url: "https://polygon-mainnet.public.blastapi.io", chainId: 137, accounts },
-    polygonZkevm: { url: "https://zkevm-rpc.com", chainId: 1101, accounts },
-    optimisticEthereum: { url: "https://mainnet.optimism.io", chainId: 10, accounts },
-    bsc: { url: "https://bsc-rpc.gateway.pokt.network", chainId: 56, accounts },
-
-    // opera: { url: "https://fantom-mainnet.public.blastapi.io", chainId: 250, accounts },
-    // cronos: { url: "https://evm.cronos.org", chainId: 25, accounts },
-  },
+  networks: networkData.reduce((o, network) => {
+    o[network.name] = {
+      url: network.urls.rpc,
+      chainId: network.chainId,
+      accounts: network.accounts || [ process.env.PRIVATE_KEY! ]
+    }
+    return o;
+  }, {} as NetworksUserConfig),
   etherscan: {
     apiKey: {
       mainnet: process.env.ETHERSCAN_API_KEY,
@@ -47,33 +153,16 @@ module.exports = {
       polygonZkevm: process.env.ZKEVM_POLYGONSCAN_API_KEY,
       optimisticEthereum: process.env.OPTIMISTIC_API_KEY,
       bsc: process.env.BSCSCAN_API_KEY,
-      // opera: process.env.FTMSCAN_API_KEY,
-      // cronos: process.env.CRONOSCAN_API_KEY,
+      dfk: process.env.AVASCAN_API_KEY,
     },
-    customChains: [
-      {
-        network: "cronos",
-        chainId: 25,
-        urls: { apiURL: "https://api.cronoscan.com/api", browserURL: "https://cronoscan.com/" },
-      },
-      {
-        network: "boba",
-        chainId: 288,
-        urls: {
-          apiURL: "https://api.bobascan.com/api",
-          browserURL: "https://bobascan.com",
-        },
-      },
-      {
-        network: "base",
-        chainId: 8453,
-        urls: { apiURL: "https://api.basescan.org/api", browserURL: "https://basescan.org/" },
-      },
-      {
-        network: "polygonZkevm",
-        chainId: 1101,
-        urls: { apiURL: "https://api-zkevm.polygonscan.com/api", browserURL: "https://zkevm.polygonscan.com/" },
-      },
-    ],
+    customChains: networkData.map(network => ({
+      network: network.name,
+      chainId: network.chainId,
+      urls: { apiURL: network.urls.api, browserURL: network.urls.browser },
+    }))
   },
+  tenderly: {
+    project: 'tradescrow',
+    username: 'DirtyCajunRice',
+  }
 };
